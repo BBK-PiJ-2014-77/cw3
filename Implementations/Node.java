fc6cd4cc06642ee851c837ab2442cc0708caf764
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2015-01-30 17:40:26 +0000

    Node Annotation

--- Implementations/Node.java
+++ Implementations/Node.java
@@ -5,34 +5,66 @@ package Implementations;
  */
 public class Node {
 
+    /**
+     * Container Object to store Instance and Node Instance to point to next node in the list
+     */
+
     private Object NodeObject = null;
     private Node NextNode = null;
+
+    /**
+     * Constructor Method
+     */
 
     public Node(){
 
     }
 
+    /**
+     * Constructor Method with Object to put into NodeObject
+     * @param obj
+     */
+
     public Node(Object obj){
         setObject(obj);
     }
+
+    /**
+     * Method to return Object placed in NodeObject
+     * @return NodeObject
+     */
 
     public Object getObject(){
         return NodeObject;
     }
 
+    /**
+     * Method to return Next Node
+     * @return NextNode
+     */
+
+
     public Node getNextNode(){
         return NextNode;
     }
+
+    /**
+     * Method to set new object for NodeObject
+     * @param set
+     */
 
     public void setObject(Object set){
         NodeObject = set;
     }
 
+    /**
+     * Method to set new NextNode
+     * @param set
+     */
+
+
     public void setNextNode(Node set){
         NextNode = set;
     }
-
-
-
 
 }
